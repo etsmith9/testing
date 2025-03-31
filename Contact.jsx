3dab484971c657bd27f979a8e3e5a9c4ee5bc1f9
Emily Smith <etsmith9@gmail.com>
2025-03-31 18:44:53 -0400

    Update Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,13 @@
+import '../PortfolioStyles.css';
+
+const Contact = () => {
+    return (
+        <footer>
+            <h2>Contact</h2>
+            <p>Email: john.doe@example.com</p>
+            <p>LinkedIn: linkedin.com/in.johndoe</p>
+        </footer>
+    );
+};
+
+export default Contact;
