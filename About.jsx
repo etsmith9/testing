c2a5ab51635cea54301e8afe337b52fe47447742
Emily Smith <etsmith9@gmail.com>
2025-03-31 18:44:32 -0400

    Update About.jsx

---
+++ About.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import '../PortfolioStyles.css';
+
+class About extends Component {
+    render() {
+        return (
+            <section>
+                <h2>About Me</h2>
+                <p> I am a passonate Web Developer with a love for creating dynamic and user-friendly websites. Welceome to my portfolio!</p>
+            </section>
+        );
+    }
+}
+
+export default About;
